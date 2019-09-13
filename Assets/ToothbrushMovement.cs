1727b2940bea1e03e871eb5a24284b6554133635
Necr0o0 <radekklaser@gmail.com>
2019-09-13 21:05:31 +0200

    start

---
+++ Assets/ToothbrushMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothbrushMovement : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        
+        transform.position = v3;
+
+    }
+}
