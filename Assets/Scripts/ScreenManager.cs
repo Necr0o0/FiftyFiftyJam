fa35bab996dea8e96d431b41e2d19f2d80a94bb9
TosterMocy <tostermocy@gmail.com>
2019-09-14 23:25:41 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenManager : MonoBehaviour
+{
+    public Button startButton;
+
+    public CanvasGroup canvasGroup;
+
+    public GameObject hairBrush;
+    public GameObject toothBrush;
+    public GameObject hair;
+    
+    //napisy
+    public GameObject napis1;
+    public GameObject napis2;
+    
+    private bool enableFade;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (enableFade)
+        {
+            FadeCanvasGroup();
+        }
+    }
+
+    public void HideMainMenu()
+    {
+        Debug.Log("Chowanie menu");
+        enableFade = true;
+        Invoke("StartCutscene", 1);
+    }
+
+    public void FadeCanvasGroup()
+    {
+        canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0f, 0.1f);
+
+    }
+
+    public void StartCutscene()
+    {
+        Debug.Log("Cutscenka wlaczenie");
+        hairBrush.SetActive(true);
+        toothBrush.SetActive(true);
+        hair.SetActive(true);
+        //napis1.SetActive(true);
+        //napis2.SetActive(true);
+    }
+}
